94059d29f9c7ca958b3fef70537c2fe881004070
jzee <jzee21b@gmail.com>
2020-05-12 09:44:46 +0900

    Start, Stop methods are moved to class LatteServiceServer

--- src/network/server/service/ServerService.java
+++ src/network/server/service/ServerService.java
@@ -16,88 +16,49 @@ import com.google.gson.Gson;
 
 import network.server.dao.Client;
 import network.server.test.AbstractClient;
+import network.server.test.SharedMessage;
 import network.server.test.User;
+import network.server.vo.Message;
 
 
 public class ServerService {
 	
-	private ServerSocket server;
-	private ExecutorService executor;
+	private static final Object MONITOR = new Object();
+	
 	private Map<String, Client> list = new ConcurrentHashMap<String, Client>();
 	private Gson gson = new Gson();
 	
+	private ServerService() {}
+	
+	private static class InstanceHandler {
+		public static final ServerService INSTANCE = new ServerService();
+	}
+	
+	public static ServerService getInstance() {
+		return InstanceHandler.INSTANCE;
+	}
 	
 	//
-	//
-	public void startServer() {
-		executor = Executors.newCachedThreadPool();
-		
-		try {
-			server = new ServerSocket();
-			server.bind(new InetSocketAddress(55566));
-			server.setSoTimeout(3000);
-		} catch (IOException e) {
-			e.printStackTrace();
-			if(!server.isClosed()) {
-				stopServer();
-			}
-		}
-		
-		System.out.println("Ready to accept...");
-		Runnable runnable = () -> {
-			Socket socket = null;
-			while(true) {
-				try {
-					socket = server.accept();
-					
-					System.out.println("[" + socket.getInetAddress() + "] connect");
-					
-					Client user = new Client(socket);
-//					user.setSocket(socket);
-					list.put(user.getDeviceID(), user);
-					executor.submit(user);
-					
-//					Tester tester = new Tester(socket);
-//					
-//					list.put(tester.hashCode(), tester);
-//					executor.submit(tester);
-					
-				} catch (SocketTimeoutException e) {
-					if(Thread.interrupted()) {
-						break;
-					} else continue;
-				} catch (IOException e) {
-//					e.printStackTrace();
-					break;
-				}
-			}
-			stopServer();
-		};
-		executor.submit(runnable);
-	} // startServer();
+	public void add(Client c) {
+		this.list.put(c.getDeviceID(), c);
+	}
 	
-	public void stopServer() {
-		try {
-			for(String key : list.keySet()) {
-				Client t = list.get(key);
-				t.close();
-				list.remove(key);
-			}
-			if(server != null && !server.isClosed()) {
-				server.close();
-			}
-			if(executor != null && !executor.isShutdown()) {
-				executor.shutdown();
-				do {
-					if (executor.isTerminated()) {
-						List<Runnable> list = executor.shutdownNow();
-						System.out.println(list.size() + " job is alive...");
-					}
-				} while (!executor.awaitTermination(10, TimeUnit.SECONDS));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	} // stopServer()
+	public void remove(Client c) {
+		this.list.remove(c.getDeviceID());
+	}
+	
+	public Client get(String id) {
+		return this.list.get(id);
+	}
+	
+	public Map<String, Client> getList() {
+		return this.list;
+	}
+	
+	public void dataHandler(String jsonData) {
+		Message data = gson.fromJson(jsonData, Message.class);
+		System.out.println(data.getDeviceID() + " : " + data.getJsonData());
+		
+	}
 	
 }
