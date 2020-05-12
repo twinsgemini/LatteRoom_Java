b3196eba548a601c9d94ae7657c5703490b79ea7
jzee <jzee21b@gmail.com>
2020-05-12 09:42:45 +0900

    Variable name change, method creation

--- src/network/server/vo/Message.java
+++ src/network/server/vo/Message.java
@@ -2,6 +2,32 @@ package network.server.vo;
 
 public class Message {
 	private String deviceID;
-	private String deviceType;
-	private SensorData sensorData;
+	private String voType;
+	private String jsonData;
+	
+	public Message(String id, String type, String data) {
+		this.deviceID = id;
+		this.voType = type;
+		this.jsonData = data;
+	}
+	
+	public String getDeviceID() {
+		return deviceID;
+	}
+	public void setDeviceID(String deviceID) {
+		this.deviceID = deviceID;
+	}
+	public String getVoType() {
+		return voType;
+	}
+	public void setVoType(String voType) {
+		this.voType = voType;
+	}
+	public String getJsonData() {
+		return jsonData;
+	}
+	public void setJsonData(String jsonData) {
+		this.jsonData = jsonData;
+	}
+	
 }
