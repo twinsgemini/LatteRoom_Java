4553e8da3e7ce7a534fe91062e332b6c689e141e
jzee <jzee21b@gmail.com>
2020-05-12 17:47:10 +0900

    update

--- src/network/server/vo/Message.java
+++ src/network/server/vo/Message.java
@@ -1,33 +1,66 @@
 package network.server.vo;
 
+import com.google.gson.Gson;
+
+import network.server.dao.Device;
+
 public class Message {
 	private String deviceID;
 	private String voType;
 	private String jsonData;
+	private static Gson gson = new Gson();
 	
-	public Message(String id, String type, String data) {
-		this.deviceID = id;
-		this.voType = type;
-		this.jsonData = data;
+	// constructor
+	private Message() {
+        this.deviceID = Device.getDeviceID();
+    }
+	
+	public Message(SensorData data) {
+        this();
+        this.voType = "SensorData";
+        this.jsonData = Message.gson.toJson(data);
+    }
+	
+	public Message(Alert data) {
+        this();
+        this.voType = "Alert";
+        this.jsonData = Message.gson.toJson(data);
+    }
+	
+	public Message(String states, String stateDetail) {
+		this();
+		this.voType = "Request";
+		this.jsonData = Message.gson.toJson(new SensorData(states, stateDetail));
 	}
 	
+	// get, set method
 	public String getDeviceID() {
 		return deviceID;
 	}
+
 	public void setDeviceID(String deviceID) {
 		this.deviceID = deviceID;
 	}
+
 	public String getVoType() {
 		return voType;
 	}
+
 	public void setVoType(String voType) {
 		this.voType = voType;
 	}
+
 	public String getJsonData() {
 		return jsonData;
 	}
+
 	public void setJsonData(String jsonData) {
 		this.jsonData = jsonData;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [deviceID=" + deviceID + ", voType=" + voType + ", jsonData=" + jsonData + "]";
 	}
 	
 }
--- src/network/server/vo/SensorData.java
+++ src/network/server/vo/SensorData.java
@@ -1,10 +1,30 @@
 package network.server.vo;
 
+import java.sql.Date;
+
 public class SensorData {
 	private int dataID;
 	private String sensorID;
-//	private String sensorType;
-	private String eventTime;
+	private Date time;
 	private String states;
 	private String stateDetail;
+	
+	
+	//
+	private SensorData() {
+		this.dataID = this.hashCode();
+		this.time = new Date(System.currentTimeMillis());
+	}
+	
+	public SensorData(String states) {
+		this();
+		this.states = states;
+	}
+	
+	public SensorData(String states, String stateDetail) {
+		this(states);
+		this.stateDetail = stateDetail;
+	}
+	
+	
 }
