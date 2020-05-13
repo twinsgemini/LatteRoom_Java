a5e03bd8ee156cb0bde5c102315bf7f8600ac4d1
jzee <jzee21b@gmail.com>
2020-05-13 15:54:54 +0900

    Arduino float data test

---
+++ src/network/server/test/FloatTest.java
@@ -0,0 +1,22 @@
+package network.server.test;
+
+public class FloatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 20;
+		float num2 = 20.2f;
+		
+		if(num1 < num2) {
+			System.out.println("num2 big");
+		} else {
+			System.out.println("Same??");
+		}
+		
+		float result1 = num2-num1;
+		System.out.println(result1);
+
+	}
+
+}
